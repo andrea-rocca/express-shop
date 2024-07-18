8491d0fde5913d041dff4584f74681d33d503893
Andrea Rocca <andrearocca83@gmail.com>
2024-07-18 11:38:36 +0200

    dynamic content with pug, ignoring node_modules

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const rootDir = require('./util/path');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -23,7 +26,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: '404 | Page Not Found', path: '/404'});
 });
 
 app.listen(PORT);
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {prods: products, pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
 router.post('/product', (req, res, next) => {
