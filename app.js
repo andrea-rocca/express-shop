bd728954144113731b91b387b6b2474dbdd1c946
Andrea Rocca <andrearocca83@gmail.com>
2024-07-18 10:46:18 +0200

    sharing data between routes in variables

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const rootDir = require('./util/path');
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,7 +19,7 @@ app.use('*', (req, res, next) => {
 });
 */
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
